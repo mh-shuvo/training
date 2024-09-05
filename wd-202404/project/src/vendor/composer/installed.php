f14bd8d4612bfbfaf34ad238100e626673391198
MD. Mehedi Hasan <shuvo@asl.aero>
2024-09-05 10:37:24 +0600

    updating

---
+++ wd-202404/project/src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bitbyteplay/wdp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b07e0c33ca889233ba5472b7f633572637c10a13',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../project/',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bitbyteplay/wdp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b07e0c33ca889233ba5472b7f633572637c10a13',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../project/',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
